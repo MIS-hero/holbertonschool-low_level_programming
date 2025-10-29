771002bbf1681d1a70dad222944c86b99279c305
Mis-hero <12423@holbertonstudents.com>
2025-10-29 17:05:44 +0000

    1

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memcpy - copies a number of bytes from a place to another
+ * @n: number of bytes 
+ * @src: copy from
+ * @dest: paste here 
+ *
+ * Return: pointer to dest 
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i]
+	}
+	return (*dest);
+}
