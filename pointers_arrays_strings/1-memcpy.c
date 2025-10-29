263a877a06be4e7b20d5791041eb4492cedfd6a4
Mis-hero <12423@holbertonstudents.com>
2025-10-29 17:21:09 +0000

    1

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -13,7 +13,9 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 
 	for (i = 0; i < n; i++)
 	{
-		dest[i] = 'src[i]';
+		*dest = *src;
+		dest++;
+		src++;
 	}
-	return (dest);
+	return (dest - n);
 }
