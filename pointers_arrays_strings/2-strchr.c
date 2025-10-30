5efd1cbb778954c754fadbe5dc107a3f87a332e6
Aljawharah <aljawharhs18@gmail.com>
2025-10-30 16:06:48 +0300

    2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -8,15 +8,20 @@
  * Return: a pointer to the first occurrence of the character c
  *         in the string s, or NULL if the character is not found
  */
+
 char *_strchr(char *s, char c)
 {
 while (*s)
 {
 if (*s == c)
+{
 return (s);
 s++;
 }
+}
 if (*s == c)
+{
 return (s);
-return (NULL);
+}
+return (0);
 }
