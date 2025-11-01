79234d6732ff9c1317b9b4b70be799a3114b105d
Aljawharah <aljawharhs18@gmail.com>
2025-11-01 23:35:23 +0300

    0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -2,13 +2,12 @@
 #include <stdio.h>
 /**
 * print_name - prints the name of the program
-* @argc: argument count
 * @argv: argument vector
 *
 * Return: 0
 */
 
-int print_name(int argc, char *argv[])
+int print_name(char *argv[])
 {
 printf("%s\n", argv[0]);
 return (0);
