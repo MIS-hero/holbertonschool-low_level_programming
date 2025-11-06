11fdca4c848531b8a26a5d9b3e0ab70ce8ec39c3
Aljawharah <aljawharhs18@gmail.com>
2025-11-06 04:07:07 +0300

    m

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* puts2 - prints every other character of a string
+* @str: the string to print from
+*
+* Return: void
+*/
+
+void puts2(char *str)
+{
+	for (int i = 0; str[i] != '\0'; i++)
+
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
