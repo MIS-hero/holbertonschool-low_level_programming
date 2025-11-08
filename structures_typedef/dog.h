748fc9f2e105e83315a7a08a5441843a761b1796
Aljawharah <aljawharhs18@gmail.com>
2025-11-08 22:28:20 +0300

    dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - A structure to represent a dog
+* @name: The name of the dog
+* @age: The age of the dog
+* @owner: The owner of the dog
+*/
+struct dog
+{
+	char *name;
+
+	float age;
+
+	char *owner;
+
+};
+typedef struct dog dog_t;
+
+#endif
