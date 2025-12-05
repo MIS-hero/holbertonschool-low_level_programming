037da3f510e1442621cb10431a67aac246887324
Aljawharah <aljawharhs18@gmail.com>
2025-12-05 19:00:46 +0300

    print list

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* print_list - Prints all the elements of a list_t list
+* @h: Pointer to the first node
+*
+* Return: Number of nodes
+*/
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%u] %s\n", h->len, h->str);
+
+		count++;
+		h = h->next; /* move to next bead on the necklace */
+	}
+
+	return (count);
+}
