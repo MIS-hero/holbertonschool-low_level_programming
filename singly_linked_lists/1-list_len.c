afbf9d780bac2522d56453c0a158f17ef88e9d43
Aljawharah <aljawharhs18@gmail.com>
2025-12-05 19:15:37 +0300

    List length- betty

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -4,14 +4,15 @@
 * @h: pointer to the head of the list
 * Return: number of elements in the list
 */
-size_t list_len(const list_t *h) 
+size_t list_len(const list_t *h)
 {
-    size_t count = 0;
+	size_t count = 0;
 
-    while (h != NULL) {
-        count++;
-        h = h->next;
-    }
+	while (h != NULL)
+	{
+		count++;
+		h = h->next;
+	}
 
-    return count;
+	return (count);
 }
