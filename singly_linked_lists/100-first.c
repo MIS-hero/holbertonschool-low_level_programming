5446ac9f5fbd791bdb19ac08eb5d66ee6f6cb923
Aljawharah <aljawharhs18@gmail.com>
2025-12-07 01:21:01 +0300

    advance-first.c

--- singly_linked_lists/100-first.c
+++ singly_linked_lists/100-first.c
@@ -2,6 +2,8 @@
 /**
 * print_before_main - Prints a message before the main function is executed
 */
+void print_before_main(void) __attribute__((constructor));
+
 void print_before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
