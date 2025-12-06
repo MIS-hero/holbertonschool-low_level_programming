3c8667f9a428af04437b75b3fc7407f7fd3c36ad
Aljawharah <aljawharhs18@gmail.com>
2025-12-07 01:05:54 +0300

    add node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -17,7 +17,8 @@ list_t *add_node(list_t **head, const char *str)
 		free(new_node);
 		return (NULL);
 	}
-	new_node->len = strlen(str);
+	while (str[len])
+		len++;
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
