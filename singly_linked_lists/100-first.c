47112b9074649ea5b56bf1d19c31402d298d459d
Aljawharah <aljawharhs18@gmail.com>
2025-12-07 01:19:20 +0300

    advance-first.c

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+* print_before_main - Prints a message before the main function is executed
+*/
+void print_before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
