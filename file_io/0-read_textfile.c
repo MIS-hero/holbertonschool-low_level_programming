8259cf019c26f748f3ecf73f356867509b496d22
Aljawharah <aljawharhs18@gmail.com>
2025-12-07 23:08:48 +0300

    file I/O - 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -10,21 +10,27 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 
 {
+	int fd;
+	char *buffer;
+	ssize_t bytes_read;
+	ssize_t bytes_written;
+
+
 	if (filename == NULL)
 		return (0);
 
-	int fd = open(filename, O_RDONLY);
+	fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
 		return (0);
-	char *buffer = malloc(sizeof(char) * (letters));
+	buffer = malloc(sizeof(char) * (letters));
 
 	if (buffer == NULL)
 	{
 		close(fd);
 		return (0);
 	}
-	ssize_t bytes_read = read(fd, buffer, letters);
+	bytes_read = read(fd, buffer, letters);
 
 	if (bytes_read == -1)
 	{
@@ -32,7 +38,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
-	ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 
 	if (bytes_written == -1 || bytes_written != bytes_read)
 	{
