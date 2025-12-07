0670fc5957d502f7b2eb37569d2a835243c19c1e
Aljawharah <aljawharhs18@gmail.com>
2025-12-07 22:58:41 +0300

    file I/O

---
+++ file_io/0-read_textfile.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+* read_textfile - reads a text file and prints it to the POSIX standard output.
+* @filename: pointer to the name of the file to be read.
+* @letters: number of letters it should read and print.
+* Return: actual number of letters it could read and print.
+*         0 if the file cannot be opened or read,
+*/
+ssize_t read_textfile(const char *filename, size_t letters)
+
+{
+if (filename == NULL)
+	return (0);
+int fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+	char *buffer = malloc(sizeof(char) * letters);
+
+if (buffer == NULL)
+{
+	close(fd);
+	return (0);
+}
+ssize_t read_bytes = read(fd, buffer, letters);
+if (read_bytes == -1)
+{
+	free(buffer);
+	close(fd);
+	return (0);
+}
+ssize_t written = write(STDOUT_FILENO, buffer, read_bytes);
+if (written != read_bytes)
+{
+	free(buffer);
+	close(fd);
+	return (0);
+}
+free(buffer);
+close(fd);
+return (written);
+
+}
---
+++ file_io/1-create_file.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+* create_file - creates a file and writes text to it.
+* @filename: pointer to the name of the file to be created.
+* @text_content: pointer to the string to write to the file.
+* Return: 1 on success, -1 on failure.
+*/
+int create_file(const char *filename, char *text_content)
+{
+	if (filename == NULL)
+		return (-1);
+
+	int fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (fd == -1)
+		return (-1);
+
+	ssize_t bytes_written = 0;
+
+	if (text_content != NULL)
+	{
+		size_t len = 0;
+
+		while (text_content[len] != '\0')
+			len++;
+
+		bytes_written = write(fd, text_content, len);
+		if (bytes_written == -1 || (size_t)bytes_written != len)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+
+	close(fd);
+	return (1);
+}
